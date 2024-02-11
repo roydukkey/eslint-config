e24103281b5bbabdf1afe21a28505311488ef864
roydukkey <contact@changelog.me>
2024-02-11 00:39:08 -0500

    Update eslint-plugin-vue

--- src/language/vue.cjs
+++ src/language/vue.cjs
@@ -23,11 +23,24 @@ module.exports = {
 				'RENDER_MODIFIERS'
 			]
 		}],
-		'vue/component-definition-name-casing': 'error',
-		'vue/component-tags-order': ['error', {
+		...rule.override('vue/component-tags-order', 'vue/block-order', ['error', {
 			order: ['template', 'script[setup]', 'style', 'script:not([setup])']
+		}]),
+		'vue/component-definition-name-casing': 'error',
+		'vue/component-name-in-template-casing': 'error',
+		'vue/component-options-name-casing': 'error',
+		'vue/custom-event-name-casing': ['error', 'kebab-case', {
+				ignores: ['/^[a-z]+(?:-[a-z]+)*:[a-z]+(?:-[a-z]+)*$/u']
 		}],
+		'vue/define-macros-order': ['error', {
+			order: ['defineOptions', 'defineProps', 'defineSlots', 'defineEmits', 'defineModel'],
+			defineExposeLast: true
+		}],
+		'vue/define-emits-declaration': ['error', 'type-literal'],
 		'vue/dot-notation': 'error',
+		'vue/enforce-style-attribute': ['error', {
+			allow: ['module', 'plain']
+		}],
 		'vue/html-end-tags': 'error',
 		'vue/html-self-closing': ['error', {
 			html: {
@@ -39,20 +52,32 @@ module.exports = {
 			singleline: Number.POSITIVE_INFINITY,
 			multiline: 1
 		}],
+		'vue/no-console': 'error',
 		'vue/no-constant-condition': 'error',
+		'vue/no-deprecated-model-definition': 'error',
+		'vue/no-duplicate-attr-inheritance': 'error',
 		'vue/no-empty-pattern': 'error',
 		'vue/no-irregular-whitespace': 'error',
 		'vue/no-loss-of-precision': 'error',
+		'vue/no-root-v-if': 'error',
 		'vue/no-sparse-arrays': 'error',
 		'vue/no-static-inline-styles': 'error',
 		'vue/no-template-shadow': 'error',
 		'vue/no-template-target-blank': 'error',
+		'vue/no-unused-emit-declarations': 'error',
 		'vue/one-component-per-file': 'error',
 		'vue/prop-name-casing': 'error',
 		'vue/require-explicit-emits': 'error',
+		'vue/require-macro-variable-name': 'error',
 		'vue/require-prop-types': 'error',
+		'vue/require-typed-object-prop': 'error',
+		'vue/require-typed-ref': 'error',
 		'vue/singleline-html-element-content-newline': 'off',
 		'vue/this-in-template': 'error',
+		'vue/v-bind-style': ['error', 'shorthand', {
+			sameNameShorthand: 'always'
+		}],
+		'vue/v-if-else-key': 'error',
 		'vue/v-slot-style': 'error',
 		...rule.inherit(require('./es.cjs').rules, {
 			'vue/eqeqeq': 'eqeqeq',
